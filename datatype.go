89ca14e23b1393245fee98d52508bfb0221eabf6
sulaimangari <sulaimangari97@gmail.com>
2022-09-14 14:10:09 +0700

    add datatype

---
+++ datatype.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Println("Satu", 1)
+	fmt.Println("Dua", 2)
+	fmt.Println("Tiga setengah", 3.5)
+	fmt.Println("Benar =", true)
+	fmt.Println("Salah =", false)
+	fmt.Println(len("Golang"))
+	fmt.Println("Let's learn Golang"[1])
+	fmt.Println("Golang is pretty fun"[3])
+}
